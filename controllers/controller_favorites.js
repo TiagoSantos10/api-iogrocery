7978006eb0ee3e576d924edd5f7d0ae5f4e5a4ba
Tiago Santos <tiagoserafimsousasantos@hotmail.com>
2022-01-22 18:25:33 +0000

    favorites changes

--- controllers/controller_favorites.js
+++ controllers/controller_favorites.js
@@ -1,12 +1,33 @@
-const favorite = require("../models/model_favorites"); 
+const favorite = require("../models/model_favorites");
+const product = require("../models/model_product");
 
-const getAllUserFavorites = (req, res) => {
-    favorite.find({card: req.params.id}, function( err, favorites) {
+
+const getAllUserFavorites = async (req, res) => {
+    /* favorite.find({card: req.params.id}, function( err, favorites) {
+        let productsArray = [];
         if (err) {
             res.status(400).send(err);
         }
-        res.status(200).json(favorites);
-    })
+        for (const fav of favorites) {
+            productsArray.push(product.filter(p => p._id === `ObjectId(${fav.product})`))  
+        }
+        res.status(200).json(productsArray);
+    }) */
+
+    let productsArray = [];
+    let favorites = await favorite.find({card: req.params.id});
+    console.log(favorites);
+    if (favorites.length == 0) {
+        res.status(404).json("User has no favorites.");
+    }
+    
+    let products = {}
+    for (const fav of favorites) {
+        products = await product.findById(fav.product);
+        productsArray.push(products)
+    }
+    
+    res.status(200).json(productsArray)
 }
 
 const addFavorite = (req, res) => {
