f2aec6c4559c8081844d5509a96e54f1208f816d
Tiago Santos <tiagoserafimsousasantos@hotmail.com>
2022-01-23 11:31:27 +0000

    final comments

--- routes/route_calories.js
+++ routes/route_calories.js
@@ -4,11 +4,11 @@ var controller_calories = require('../controllers/controller_calories')
 var utilities = require('../utilities/utilities')
 
 
-/* 
+
 router.route('/')
     .post(controller_calories.getProductCalories)
-    .put(controller_calories.addPortfirProduct)
+    /* .put(controller_calories.addPortfirProduct) */
 
- */
+
 
 module.exports = router
